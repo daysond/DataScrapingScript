836775eebf8681cdb728f0bc8a34215dfeb65265
Yiyuan Dong <daysond@outlook.com>
2019-10-03 00:42:00 -0400

    Initial Commit

---
+++ LevelGetter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LevelGetter
+//
+//  Created by Dayson Dong on 2019-10-03.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
